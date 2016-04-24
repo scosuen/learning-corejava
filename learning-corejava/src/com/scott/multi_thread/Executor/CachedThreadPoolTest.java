5bd3b4abb25c5cdc4fac874bb653a53acaf4921e
Scott <sco.suen.lab@gmail.com>
2016-04-23 21:14:20 -0400

    enha:CachedTreadPool

--- learning-corejava/src/com/scott/multi_thread/Executor/CachedThreadPoolTest.java
+++ learning-corejava/src/com/scott/multi_thread/Executor/CachedThreadPoolTest.java
@@ -9,15 +9,12 @@ public class CachedThreadPoolTest {
 //      ExecutorService executorService = Executors.newFixedThreadPool(5);  
 //      ExecutorService executorService = Executors.newSingleThreadExecutor();  
         for (int i = 0; i < 5; i++){   
-            executorService.execute(new TestRunnable());   
+            executorService.execute(() -> {
+            	System.out.println(Thread.currentThread().getName() + "线程被调用了。");   
+            	
+            });   
             System.out.println("*************" + i + " *************");   
         }   
         executorService.shutdown();   
     }   
 }
-
-class TestRunnable implements Runnable{   
-    public void run(){   
-        System.out.println(Thread.currentThread().getName() + "线程被调用了。");   
-    }   
-}  
