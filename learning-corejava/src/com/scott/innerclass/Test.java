be6f7785acad16e50568c3d78c57d04c527ff5e7
Scott <sco.suen.lab@gmail.com>
2016-07-03 23:44:10 -0400

    Feat: new inner class

--- learning-corejava/src/com/scott/innerclass/Test.java
+++ learning-corejava/src/com/scott/innerclass/Test.java
@@ -1,10 +1,15 @@
 package com.scott.innerclass;
 
+import com.scott.innerclass.OuterClass.InnerClass;
+
 public class Test {
 
 	public static void main(String[] args) {
 		OuterClass outerClassTest = new OuterClass();
 		System.out.println(outerClassTest.getInnerInstance().getInnerString());
+		
+		InnerClass innerClass = new OuterClass().new InnerClass();
+		
 	}
 
 }
